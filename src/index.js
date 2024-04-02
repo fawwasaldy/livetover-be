b527eff685c522a1313efb87a593502e088c87ac
fawwasaldy <fawwas.aldy2111@gmail.com>
2024-04-02 14:32:16 +0700

    build: nodeJS, express, & mongoDB setup

---
+++ src/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.URL_DATABASE;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error);
+});
+
+database.once('connected', () => {
+  console.log('Database connected');
+});
+
+const app = express();
+
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
